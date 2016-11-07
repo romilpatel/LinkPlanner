a0e50e3f074c0006c47a3246bcd9635da1ac1f70
anpinto <anp@ua.pt>
2016-11-07 15:51:03 +0000

    im_modulator

--- sdf/imdd/imdd_sdf.cpp
+++ sdf/imdd/imdd_sdf.cpp
@@ -3,6 +3,7 @@
 # include "binary_source.h"
 # include "discrete_to_continuous_time.h"
 # include "pulse_shaper.h"
+# include "im_modulator.h"
 # include "sink.h"
 
 int main() {
@@ -17,6 +18,8 @@ int main() {
 
 	TimeContinuousAmplitudeContinuousReal S3{ "S3.sgn" };
 
+	OpticalSignal S4{ "S4.sgn" };
+
 	// #####################################################################################################
 	// ########################### Blocks Declaration and Inicialization ###################################
 	// #####################################################################################################
@@ -25,7 +28,7 @@ int main() {
 	BinarySource B1{ vector<Signal*> {}, vector<Signal*> { &S1 } };
 	B1.setMode(DeterministicAppendZeros);
 	B1.setBitPeriod(1.0 / 50e9);
-	B1.setBitStream("00100");
+	B1.setBitStream("0000000001000000000");
 	//B1.setPatternLength(5);
 	//B1.setNumberOfBits(10000);
 
@@ -37,15 +40,18 @@ int main() {
 	B3.setImpulseResponseTimeLength(16);
 	B3.setSeeBeginningOfImpulseResponse(false);
 
-	Sink B4{ vector<Signal*> { &S3 }, vector<Signal*> {} };
-	B4.setNumberOfSamples(5000);
-	B4.setDisplayNumberOfSamples(true);
+	ImModulator B4{ vector<Signal*> { &S3 }, vector<Signal*> { &S4 } };
+	B4.outputOpticalPower = 1e-3;
+
+	Sink B5{ vector<Signal*> { &S4 }, vector<Signal*> {} };
+	B5.setNumberOfSamples(5000);
+	B5.setDisplayNumberOfSamples(true);
 
 	// #####################################################################################################
 	// ########################### System Declaration and Inicialization ###################################
 	// #####################################################################################################
 
-	System MainSystem{ vector<Block*> { &B1, &B2, &B3, &B4 } };
+	System MainSystem{ vector<Block*> { &B1, &B2, &B3, &B4, &B5 } };
 
 	// #####################################################################################################
 	// #################################### System Run #####################################################

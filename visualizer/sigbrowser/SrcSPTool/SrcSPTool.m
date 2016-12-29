dc00d875a4698894f7854fe56be177086bf2b48b
Armando Humberto <Armando Humberto>
2016-12-29 11:41:53 +0000

    2016-12-29

--- visualizer/sigbrowser/SrcSPTool/SrcSPTool.m
+++
@@ -1,9 +0,0 @@
-function this = SrcSPTool(varargin)
-% Constructor for SPTool-based datasource
-
-% Copyright 2012 The MathWorks, Inc.
-
-this = sigbrowser.SrcSPTool;
-this.initSource(varargin{:});
-
-% [EOF]

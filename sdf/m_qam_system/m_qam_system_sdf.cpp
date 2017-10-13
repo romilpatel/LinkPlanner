3935efde5b3d3b0fa172fc1dd823a86fc8758480
NetXPTO <netxpto@gmail.com>
2017-10-13 14:21:47 +0100

    2017-10-13

--- sdf/m_qam_system/m_qam_system_sdf.cpp
+++ sdf/m_qam_system/m_qam_system_sdf.cpp
@@ -9,38 +9,35 @@ int main(){
 
 	// #####################################################################################################
 	// #################################### System Input Parameters ########################################
-<<<<<<< HEAD
 	// #####################################################################################################
 
-	t_integer numberOfBitsGenerated(40);
+	t_integer numberOfBitsGenerated(1000);
 	t_integer samplesPerSymbol(32);
 	t_integer pLength = 5;
 	t_real bitPeriod = 1.0 / 50e9;
 	t_real rollOffFactor = 0.3;
-	vector<t_iqValues> iqAmplitudeValues = { { -1, 0 },{ 1, 0 } };
+	//vector<t_iqValues> iqAmplitudeValues = { { -1, 0 },{ 1, 0 } };
 	t_real signalOutputPower_dBm = -20;
 
 
 	// #####################################################################################################
 	// ########################### Signals Declaration and Inicialization ##################################
-=======
->>>>>>> AnaLuisa
 	// #####################################################################################################
 
 	int numberOfBitsReceived(-1);
-	int numberOfBitsGenerated = 3000;
-	double bitPeriod = 1.0 / 50e9;
+	//int numberOfBitsGenerated = 10;
+	//double bitPeriod = 1.0 / 50e9;
 
 	int prbsPatternLength = 5;
 
-	vector<t_iqValues> iqAmplitudeValues = { { 1.0, 1.0 },{ -1.0, 1.0 },{ 1.0, -1.0 },{ -1.0, -1.0 } };
+	vector<t_iqValues> iqAmplitudeValues = { { 1.0, 1.0 },{ -1.0, 1.0 },{ -1.0, -1.0 },{ 1.0, -1.0 } };
 	
-	double rollOffFactor = 0.3;
+	//double rollOffFactor = 0.3;
 
-	int samplesPerSymbol = 16;
+	//int samplesPerSymbol = 16;
 	double symbolPeriod = bitPeriod / samplesPerSymbol;
 
-	t_real signalOutputPower_dBm = -20; 
+	//t_real signalOutputPower_dBm = -20; 
 	t_real localOscillatorPower_dBm = 0; 
 	t_real localOscillatorPhase = 0;
 	//array<t_complex, 4> transferMatrix = { { 1 / sqrt(2), 1 / sqrt(2), 1 / sqrt(2), -1 / sqrt(2)} };
@@ -48,11 +45,12 @@ int main(){
 	t_real amplification = 1;
 	t_real noiseAmplitude = 1*pow(10,-6);
 	//t_integer samplesToSkip = 0;
-	t_integer samplesToSkip = 2 * 8 * samplesPerSymbol; //+ floor(samplesPerSymbol / 2);
+	t_integer samplesToSkip = 0;
+	//t_integer samplesToSkip = 2 * 8 * samplesPerSymbol; //+ floor(samplesPerSymbol / 2);
 	//8 is the number of samples used by the filter
 	t_real confidence = 0.95;
 	t_integer midReportSize = 0;
-	t_integer bufferLength = 256;
+	t_integer bufferLength = 13;
 	
 	//double clockPeriod = symbolPeriod;
 	//double samplingPeriod = 16;
@@ -95,11 +93,11 @@ int main(){
 	B2.setIqAmplitudes(iqAmplitudeValues);
 	B2.setLocalOscillatorOpticalPower_dBm(localOscillatorPower_dBm);
 	B2.setLocalOscillatorPhase(localOscillatorPhase);
-	B2.setSamplerOpticalPower_dBm(signalOutputPower_dBm);
+	//B2.setSamplerOpticalPower_dBm(signalOutputPower_dBm);
 	//B2.setTransferMatrix(transferMatrix);
 	B2.setResponsivity(responsivity);
-	B2.setAmplification(amplification);
-	B2.setNoiseAmplitude(noiseAmplitude);
+	//B2.setAmplification(amplification);
+	//B2.setNoiseAmplitude(noiseAmplitude);
 	B2.setSamplesToSkip(samplesToSkip);
 	//B2.setPosReferenceValue(0);
 	//B2.setNegReferenceValue(0);
@@ -114,11 +112,10 @@ int main(){
 	B3.setMidReportSize(midReportSize);
 
 	Sink B4{ vector<Signal*> { &S3 }, vector<Signal*> {} };
-	B4.setNumberOfSamples(numberOfBitsReceived);
+	B4.setNumberOfSamples(numberOfBitsReceived*samplesPerSymbol);
 	B4.setDisplayNumberOfSamples(true);
 
-<<<<<<< HEAD
-	MQamTransmitter B1{ vector<Signal*> {}, vector<Signal*> { &S1, &S2 } };
+	/*MQamTransmitter B1{ vector<Signal*> {}, vector<Signal*> { &S1, &S2 } };
 	B1.setNumberOfBits(numberOfBitsGenerated);
 	B1.setOutputOpticalPower_dBm(signalOutputPower_dBm);
 	//B1.setMode(PseudoRandom);
@@ -131,31 +128,19 @@ int main(){
 	B1.setRollOffFactor(rollOffFactor);
 	B1.setSaveInternalSignals(true);
 	B1.setSeeBeginningOfImpulseResponse(true);
-=======
+
 	//Without BER measurement
 	//Sink B3{ vector<Signal*> { &S0 }, vector<Signal*> {} };
 	//B3.setNumberOfSamples(numberOfBitsReceived*samplesPerSymbol);
 	//B3.setDisplayNumberOfSamples(true);
->>>>>>> AnaLuisa
+
 
 	//Sink B4{ vector<Signal*> { &S2 }, vector<Signal*> {} };
 	//B4.setNumberOfSamples(numberOfBitsReceived*samplesPerSymbol);
 	//B4.setDisplayNumberOfSamples(true);
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> Romil
 	Sink B3{ vector<Signal*> { &S2 }, vector<Signal*> {} };
-	B3.setDisplayNumberOfSamples(true);
-=======
->>>>>>> develop
-<<<<<<< HEAD
-=======
->>>>>>> 526292907dd2ff7d6ea618152856721b6b80e5dd
-=======
->>>>>>> Romil
+	//B3.setDisplayNumberOfSamples(true);*/
 
 	// #####################################################################################################
 	// ########################### System Declaration and Inicialization ###################################

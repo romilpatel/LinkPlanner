868c3583c62a7bc0df5b570f9dde9e6743b697dd
NetXPTO <netxpto@gmail.com>
2017-10-13 15:27:34 +0100

    2017-10-13, before meeting Diamantino

--- include/photodiode.h
+++ include/photodiode.h
@@ -9,33 +9,32 @@ class Photodiode : public Block {
 
 	bool firstTime{ true };
 	int aux = 0;
-	//bool firstPass{ true };
 	double t = 0;
 
+	double responsivity{ 1 };
+
+	bool shotNoise{ false };
+
+	t_real thermalNoiseAmplitude{ 0.0 };
+
+
 public:
-
-	double frequencyMismatch{ 1.9441e+11 };
-	double responsivity = 1;
-	bool shotNoise = false;
-
-	default_random_engine generatorAmp1;
-	default_random_engine generatorAmp2;
 
 	Photodiode() {};
 	Photodiode(vector<Signal *> &InputSig, vector<Signal *> &OutputSig) :Block(InputSig, OutputSig){};
 	
-	double outputOpticalWavelength{ 1550e-9 };
-	double outputOpticalFrequency{ SPEED_OF_LIGHT / outputOpticalWavelength };
-
 	void initialize(void);
 	bool runBlock(void);
 
 	void setResponsivity(double Responsivity) { responsivity = Responsivity; }
 	double const getResponsivity(void) { return responsivity; }
+	
+	void setShotNoise(bool sNoise) { shotNoise = sNoise; }
+	bool getShotNoise(void) { return shotNoise; }
 
-	void useNoise(bool sNoise) { shotNoise = sNoise; }
+	void setThermalNoiseAmplitude(double amplitude) { thermalNoiseAmplitude = amplitude; }
+	double getThermalNoiseAmplitude(void) { return thermalNoiseAmplitude; };
 
 };
-
 
 #endif // !PROGRAM_INCLUDE_PHOTODIODE_H_
--- lib/photodiode.cpp
+++ lib/photodiode.cpp
@@ -18,57 +18,98 @@ void Photodiode::initialize(void){
 
 }
 
-bool Photodiode::runBlock(void) {
+
+bool Photodiode::runBlock(void){
 
 	double samplingPeriod = inputSignals[0]->getSamplingPeriod();
+	double symbolPeriod = inputSignals[0]->getSymbolPeriod ();
+	int samplesPerSymbol = (int)round(symbolPeriod / samplingPeriod);
 
-	int ready1 = inputSignals[0]->ready();
-	int ready2 = inputSignals[1]->ready();
-	int ready = min(ready1, ready2);
+	int ready = inputSignals[0]->ready();
 
 	int space = outputSignals[0]->space();
 
 	int process = min(ready, space);
-
+	
 	if (process == 0) return false;
 
+	if ((!getShotNoise()) & (getThermalNoiseAmplitude() == 0)) {
+
+		t_complex input1;
+		t_real power, current;
+		for (int i = 0; i < process; i++) {
+
+			t_complex input1;
+			inputSignals[0]->bufferGet(&input1);
+
+			// The 4 factor is compensating the bandpass signal representation
+			// amplitude correction.
+			power = 2 * abs(input1)*abs(input1);
+
+			current = responsivity*power;
+		}
+	};
+
+	/*
+	// Probability distribution
 	normal_distribution<double> distribution(0, 1);
-	double noiseAmp1;
-	double noiseAmp2;
+	double noiseAmpQuantum;
+	double noiseAmpThermal;
 
-	double wavelength = inputSignals[0]->getCentralWavelength();
-
-	unsigned seed = chrono::system_clock::now().time_since_epoch().count();
+	unsigned seed = (unsigned)chrono::system_clock::now().time_since_epoch().count();
 
 	generatorAmp1.seed(seed);
 	generatorAmp2.seed(seed);
 
+
+	double wavelength = inputSignals[0]->getCentralWavelength();
+	// Power constant for shotNoise.
+	// P = power of 1 photon.
+	t_real P = PLANCK_CONSTANT*SPEED_OF_LIGHT / (samplingPeriod*wavelength);
+
+	t_real power, current;
+
 	for (int i = 0; i < process; i++) {
 
-		noiseAmp1 = distribution(generatorAmp1);
-		noiseAmp2 = distribution(generatorAmp2);
+		noiseAmpQuantum = distribution(generatorAmp1);
+		noiseAmpThermal = distribution(generatorAmp2);
 
 		t_complex input1;
 		inputSignals[0]->bufferGet(&input1);
-		t_complex input2;
-		inputSignals[1]->bufferGet(&input2);
 
-		t_real power1 = abs(input1)*abs(input1) * 4;
-		t_real power2 = abs(input2)*abs(input2) * 4;
 
-		t_real current1 = responsivity*power1;
-		t_real current2 = responsivity*power2;
-		t_real out = current1 - current2;
+		// The 4 factor is compensating the bandpass signal representation
+		// amplitude correction.
+		power = abs(input1)*abs(input1) * 4;
+
 
 		if (shotNoise)
 		{
-			power1 = power1 + sqrt(PLANCK_CONSTANT*SPEED_OF_LIGHT / (samplingPeriod*wavelength))*noiseAmp1*(sqrt(power1) + sqrt(PLANCK_CONSTANT*SPEED_OF_LIGHT / (samplingPeriod*wavelength))*noiseAmp1 / 4);
-			power2 = power2 + sqrt(PLANCK_CONSTANT*SPEED_OF_LIGHT / (samplingPeriod*wavelength))*noiseAmp2*(sqrt(power2) + sqrt(PLANCK_CONSTANT*SPEED_OF_LIGHT / (samplingPeriod*wavelength))*noiseAmp2 / 4);
-			current1 = responsivity*power1;
-			current2 = responsivity*power2;
-			out = current1 - current2;
+			// This approximation assumes that the number of photons is much
+			// greater than 0.	
+			power += sqrt(P)*noiseAmpQuantum * (sqrt(power) + sqrt(P)*noiseAmpQuantum / 4);
 		}
-		outputSignals[0]->bufferPut(out);
+
+		// Conversion of optical power to current.
+		current = responsivity*power;
+		
+		
+		// Implementation of thermal noise
+		if (thermalNoise) {
+			current += thermalNoiseAmplitude * noiseAmpThermal;
+		}
+
+		outputSignals[0]->bufferPut(current);
+		
+		// Extras auxiliares (ver definição da class)
+		t = t + samplingPeriod;
+		aux++;
+		if (aux==samplesPerSymbol)
+		{
+			aux = 0;
+		}
+
 	}
+	*/
 	return true;
 }

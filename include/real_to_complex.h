46a4dd89246b882c86baca3638204c27c1867874
DiamantinoSilva <arq.diamantino@gmail.com>
2017-11-03 16:44:35 +0000

    ud-20171103-1644

---
+++ include/real_to_complex.h
@@ -0,0 +1,26 @@
+# ifndef PROGRAM_INCLUDE_REAL_TO_COMPLEX_H_
+# define PROGRAM_INCLUDE_REAL_TO_COMPLEX_H_
+
+# include "netxpto.h"
+
+
+// Creates a complex signal from two real signals
+class realToComplex : public Block {
+
+	/* State Variables */
+	bool firstTime{ true };
+
+ public:
+
+	/* Methods */
+	realToComplex() {};
+	realToComplex(vector<Signal *> &InputSig, vector<Signal *> &OutputSig) :Block(InputSig, OutputSig){};
+
+	void initialize(void);
+	bool runBlock(void);
+
+};
+
+# endif
+
+
